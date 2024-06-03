56255bb5abf80520dd1281d6324815de5b49684d
udin4719@gmail.com <udin4719@gmail.com>
2024-06-03 22:40:58 +0700

    alert

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\M.Solehuddin\OneDrive\Documents\KULIAH\SEMESTER 6\MSIB\LearningX - Essential Programming Course\Tugas\Final Project\fortissimo-project-1\venv
+set VIRTUAL_ENV=C:\Users\M.Solehuddin\OneDrive\Documents\KULIAH\SEMESTER 6\MSIB\LearningX - Essential Programming Course\Tugas\Final Project\fortissimo-project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
